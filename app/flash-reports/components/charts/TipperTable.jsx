58daaa0c353b8affaba3871160aaaff9fa531358
Arun pandian <arunpandian972000@gmail.com>
2025-08-03 16:28:10 +0530

    deploy v-49

--- app/flash-reports/components/charts/TipperTable.jsx
+++ app/flash-reports/components/charts/TipperTable.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const formatValue = (num) => {
-  if (num >= 100000) return `${(num / 100000).toFixed(1)}L`;
-  if (num >= 1000) return `${(num / 1000).toFixed(1)}K`;
-  return num.toString();
-};
-
 const ForecastTable = () => {
   const startMonth = 6; // July
   const months = Array.from({ length: 7 }, (_, i) =>
@@ -21,7 +15,7 @@ const ForecastTable = () => {
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
         <h3 style={{ marginBottom: 8 }}>Tipper Sales Performance</h3>
         <div style={{
-          fontSize: '18px',
+          fontSize: '20px',
           fontWeight: 'bold',
           color: '#ccc',
           paddingRight: '5px'
@@ -43,14 +37,12 @@ const ForecastTable = () => {
           </tr>
         </thead>
         <tbody>
-          {Object.keys(data).map((segment) => (
-            <tr key={segment}>
-              <td style={tdStyle}>{segment}</td>
-              {data[segment].map((val, idx) => (
-                <td key={idx} style={tdStyle}>{formatValue(val)}</td>
-              ))}
-            </tr>
-          ))}
+          <tr>
+            <td style={tdStyle}>Total Sales</td>
+            {data['Total Sales'].map((val, idx) => (
+              <td key={idx} style={tdStyle}>{val}</td>
+            ))}
+          </tr>
         </tbody>
       </table>
     </div>
